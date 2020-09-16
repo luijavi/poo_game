f35554bb7281d7f0bd25790f44baf5f26a0d0dec
luijavi <luijavi@gmail.com>
2020-09-16 13:02:14 -0500

    added frametimer and ft mark call

--- Engine/Game.cpp
+++ Engine/Game.cpp
@@ -50,6 +50,8 @@ void Game::Go()
 
 void Game::UpdateModel()
 {
+	const float dt = ft.Mark();
+
 	goal.UpdateColor();
 	if( isStarted && !isGameOver )
 	{
@@ -28448,4 +28450,5 @@ void Game::ComposeFrame()
 		}
 		meter.Draw( gfx );
 	}
+
 }
